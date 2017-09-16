160fe93350f011a070e44d228faae555bfe43f40
Alberto Cortés <alcortesm@gmail.com>
2017-09-16 12:56:37 +0200

    grid: basic size

--- conway/conway.go
+++ conway/conway.go
@@ -1,7 +1,7 @@
 package conway
 
 type Universe interface {
-	Status() Grid
+	Status() *Grid
 	Tick()
 }
 
@@ -10,9 +10,16 @@ type Animator interface {
 	Animate(file string)
 }
 
+// A grid represents a snapshot of a universe.
 type Grid interface {
+	// Width returns the width of the universe (number of cells).
 	Width() int
+	// Height returns the height of the universe (number of cells).
 	Height() int
+	// Get returns if the cell at coordinates x, y is alive.  Returns
+	// an error if x or y are out of bounds.
 	Get(x, y int) (bool, error)
+	// Set sets the cell at coordinates x, y to value v (true for alive,
+	// false for dead).  Returns an error if x or y are out of bounds.
 	Set(x, y int, v bool) error
 }
--- grid/grid.go
+++ grid/grid.go
@@ -2,6 +2,9 @@ package grid
 
 import "fmt"
 
+// A grid represents a snapshot of a universe.
+// The zero value of this type is not safe, use the function New below.
+// The minimum width and height for a universe is 3 cells each.
 type Grid struct {
 	width, height int
 }
@@ -11,6 +14,8 @@ const (
 	minHeight = 3
 )
 
+// New creates a new grid with the given width and height (number of cells).
+// Returns an error if the width or the height is smaller than 3.
 func New(width, height int) (*Grid, error) {
 	if width < minWidth {
 		return nil, fmt.Errorf("width must be >= than %d, was %d",
@@ -26,18 +31,28 @@ func New(width, height int) (*Grid, error) {
 	}, nil
 }
 
+// Width returns the width of the universe (number of cells).
+// Implements conway.Grid.
 func (g *Grid) Width() int {
 	return g.width
 }
 
+// Height returns the height of the universe (number of cells).
+// Implements conway.Grid.
 func (g *Grid) Height() int {
 	return g.height
 }
 
+// Get returns if the cell at coordinates x, y is alive.  Returns
+// an error if x or y are out of bounds.
+// Implements conway.Grid.
 func (g *Grid) Get(x, y int) (bool, error) {
 	return false, fmt.Errorf("TODO")
 }
 
+// Set sets the cell at coordinates x, y to value v (true for alive,
+// false for dead).  Returns an error if x or y are out of bounds.
+// Implements conway.Grid.
 func (g *Grid) Set(x, y int, v bool) error {
 	return fmt.Errorf("TODO")
 }
