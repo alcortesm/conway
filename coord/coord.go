62087ae25ee974034cafa657176f2bf5fe35a526
Esther Sanchez <sanchez.ther@gmail.com>
2017-10-05 21:23:35 +0200

    Coord & Grid documentation

--- coord/coord.go
+++ coord/coord.go
@@ -1,17 +1,25 @@
 package coord
 
+// Coord represents a cell in a grid.
 type Coord struct {
 	x, y uint
 }
 
+// New creates a new coord, representing a cell in a grid in the given
+// position.  Positions are represented as cartesian coordinates, where
+// the origin is at the upper left of the grid.
 func New(x, y uint) *Coord {
 	return &Coord{x: x, y: y}
 }
 
+// X returns the cell's abscissa.
+// Implements conway.Coord.
 func (c *Coord) X() uint {
 	return c.x
 }
 
+// Y returns the cell's ordinate.
+// Implements conway.Coord.
 func (c *Coord) Y() uint {
 	return c.y
 }
